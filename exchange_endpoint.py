0c666a598d145f731cdce6d7811d25a0587baa1b
yli310 <77583949+yli310@users.noreply.github.com>
2021-08-18 22:24:43 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -113,9 +113,9 @@ def get_eth_keys(filename = "eth_mnemonic.txt"):
     
     # TODO: Generate or read (using the mnemonic secret) 
     # the ethereum public/private keys
-    w3.eth.account.enable_unaudited_hdwallet_features()
-    acct,mnemonic_secret = w3.eth.account.create_with_mnemonic()
-    acct = w3.eth.account.from_mnemonic(mnemonic_secret)
+    g.w3.eth.account.enable_unaudited_hdwallet_features()
+    acct,mnemonic_secret = g.w3.eth.account.create_with_mnemonic()
+    acct = g.w3.eth.account.from_mnemonic(mnemonic_secret)
 
     eth_pk = acct._address
     eth_sk = acct._private_key
